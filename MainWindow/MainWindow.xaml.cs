7557a6536074c5ba2575d0eae6f79f1bd8b5b4b5
Maurycy Hartung <maurycyhartung@gmail.com>
2021-05-26 15:46:13 +0200

    Creating First rectangle + Binding to VM

--- MainWindow/MainWindow.xaml.cs
+++ MainWindow/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SnakeSense
+namespace SnakeSense.MainWindow
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +23,7 @@ namespace SnakeSense
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
---
+++ MainWindow/MainWindowViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeSense.MainWindow
+{
+
+    //ViewModel for MainWindow
+    public class MainWindowViewModel
+    {
+        private int mXPosition;
+        private int mYPosition;
+        // Property for X Position of snake
+        public int XPosition
+        {
+            get 
+            { 
+                return mXPosition; 
+            }
+            set 
+            {
+                mXPosition = value; 
+            }
+        }
+        // Property for Y Position of snake
+        public int YPosition
+        {
+            get
+            {
+                return mYPosition;
+            }
+            set
+            {
+                mYPosition = value;
+            }
+        }
+        //Default Constructor
+        public MainWindowViewModel()
+        {
+            XPosition = 150;
+            YPosition = 100;
+        }
+    }
+}
