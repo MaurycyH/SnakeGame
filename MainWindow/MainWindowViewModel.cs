a67d7dc8a042b23a01d982af0677dd934b1c887e
Maurycy Hartung <maurycyhartung@gmail.com>
2021-05-28 20:25:22 +0200

    Add border condition to lose

--- MainWindow/MainWindowViewModel.cs
+++ MainWindow/MainWindowViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using System.Windows;
 using System.Windows.Input;
 
 namespace SnakeSense.MainWindow
@@ -13,6 +14,9 @@ namespace SnakeSense.MainWindow
     //ViewModel for MainWindow
     public class MainWindowViewModel : NotifyViewModel
     {
+        private double mWindowHeight;
+        private double mWindowWidth;
+        private bool IfPause = false;
         /// <summary>
         ///  Object representing Snake
         /// </summary>
@@ -24,21 +28,50 @@ namespace SnakeSense.MainWindow
         /// <summary>
         /// Command for key input
         /// </summary>
-        public ICommand KeyCommand { get; }
+       // public ICommand KeyCommand { get; }
         /// <summary>
         /// Constructor for ViewModel
         /// </summary>
         public MainWindowViewModel()
         {
             Snake = new Snake();
-            Timer = new Timer(150);           
+            Timer = new Timer(50);
+            mWindowHeight = App.Current.MainWindow.Height;
+            mWindowWidth = App.Current.MainWindow.Width;
             Timer.Elapsed += new ElapsedEventHandler(RefreshSnakePosition);
             Timer.Enabled = true;
-
+            
         }
         public void RefreshSnakePosition(object source, ElapsedEventArgs e)
         {
-            Snake.MoveSnake(source, e);
+            if (ChceckBorder())
+            {
+                Snake.MoveSnake(source, e);
+            }
+          
+
+        }
+        public bool ChceckBorder()
+        {
+            if (!IfPause)
+            {
+                if ((Snake.XPosition >= mWindowWidth) || (Snake.YPosition >= mWindowHeight) || (Snake.XPosition <= 0) || (Snake.YPosition <= 0))
+                {
+                    Snake.YSpeed = 0;
+                    Snake.XSpeed = 0;
+                    IfPause = true;
+                    MessageBox.Show("Przegrales bo poza granice wyszedles");
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
 
         }
     }
