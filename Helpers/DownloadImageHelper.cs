894c9f3eb17926eab3c8428f76880820da3ff475
Maurycy Hartung <maurycyhartung@gmail.com>
2021-05-31 20:35:02 +0200

    Adding some class helpers

---
+++ Helpers/DownloadImageHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SnakeSense.Helpers
+{
+    public class DownloadImageHelper
+    {
+        private List<Uri> mImageList;
+        public Random random;
+        public DownloadImageHelper()
+        {
+            mImageList = new List<Uri>();
+            mImageList.Add(new Uri("https://cdn.pixabay.com/photo/2013/07/13/13/42/snake-161424_960_720.png"));
+            mImageList.Add(new Uri("https://cdn.pixabay.com/photo/2013/10/15/10/04/snake-195917_960_720.jpg"));
+            mImageList.Add(new Uri("https://cdn.pixabay.com/photo/2017/02/01/11/02/animal-2029654_960_720.png"));
+            mImageList.Add(new Uri("https://cdn.pixabay.com/photo/2016/04/01/08/40/animal-1298878_960_720.png"));
+            mImageList.Add(new Uri("https://cdn.pixabay.com/photo/2013/07/12/12/30/snake-145808_960_720.png"));
+        }
+        public async Task GetImageAsync(CancellationToken cancellationToken)
+        {
+            int rand = random.Next(4);
+            Uri imageUri = mImageList[rand];
+            using (WebClient webClient = new WebClient())
+            {
+                byte [] data = await webClient.DownloadDataTaskAsync(mImageList[rand]);
+                using (MemoryStream mem = new MemoryStream(data))
+                {
+                    System.Drawing.Image image = System.Drawing.Image.FromStream(mem);
+                    ResizeImageHelper.ResizeImage(image, 32, 32).Save(Path.Combine(Environment.SpecialFolder.ApplicationData.ToString(),"Snake",$"SnakePhoto{rand}.png"), ImageFormat.Png);
+
+                }
+            }
+        }
+    }
+}
---
+++ Helpers/ResizeImageHelper.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SnakeSense.Helpers
+{
+    /// <summary>
+    /// Resize the image to the specified width and height.
+    /// </summary>
+    /// <param name="image">The image to resize.</param>
+    /// <param name="width">The desired width.</param>
+    /// <param name="height">The desired height.</param>
+    /// <returns>The resized image.</returns>
+    public static class ResizeImageHelper
+    {
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            Rectangle destRect = new Rectangle(0, 0, width, height);
+            Bitmap destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (Graphics graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (ImageAttributes wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+    }
+}
